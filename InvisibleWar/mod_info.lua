a05e1d2ab416f91cb6775eadf9e6bbec02128442
The-Balthazar <the.balthazar@gmail.com>
2016-09-11 13:24:18 +0100

    Requestathon #4: Invisible War. Everything has cloak. No one asked for this!

---
+++ InvisibleWar/mod_info.lua
@@ -0,0 +1,13 @@
+name = "Invisible War"
+uid = "16678e1e-7fc9-11e5-8bcf-dayofsex0010"
+version = 1.0
+copyright = "© 2016 Sean Wheeldon"
+description = "Everything has cloak. Did you remember your scouts?"
+author = "Sean Wheeldon"
+icon = "/mods/Requestathon/InvisibleWar/mod_icon.dds"
+selectable = true
+enabled = true
+exclusive = false
+ui_only = false
+before = {  }
+after = {  }
